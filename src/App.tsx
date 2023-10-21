7b8fe8f51afae98930f6c5539e80b4e1b476b76a
anna <annapapros21@gmail.com>
2023-10-21 22:52:41 +0200

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material"
+
+function App() {
+
+  return (
+    <>
+      <Typography variant="h3">Obfuscator svg</Typography>
+    </>
+  )
+}
+
+export default App
