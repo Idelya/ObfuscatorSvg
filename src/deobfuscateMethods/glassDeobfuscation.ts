65c799465b2954d56a34d695c1a65c5d3f2a8e95
Mikołaj <macioszczyk.mikolaj@gmail.com>
2023-12-26 23:44:07 +0100

    glass rect from paths deobfuscation

--- src/deobfuscateMethods/glassDeobfuscation.ts
+++ src/deobfuscateMethods/glassDeobfuscation.ts
@@ -1,6 +1,6 @@
 export const revertGlass = (groupSvg: SVGGElement) => {
   revertGlassFromPolygons(groupSvg);
-  // TODO: Try revertGlassFromPaths
+  revertGlassFromPaths(groupSvg);
 };
 
 const revertGlassFromPolygons = (groupSvg: SVGGElement) => {
@@ -152,4 +152,172 @@ const getPointPolygonMapping = (polygonElements: SVGPolygonElement[]) => {
 
 const getPointsFromPolygon = (svgPolygon: SVGPolygonElement) => {
   return svgPolygon.getAttribute("points")!.split(" ");
+};
+
+const revertGlassFromPaths = (groupSvg: SVGGElement) => {
+  let pathElements: SVGPathElement[] = Array.from(
+    groupSvg.querySelectorAll("path"),
+  );
+
+  const polygonPathPattern =
+    /^M\d+(\.\d+)?,\d+(\.\d+)? \d+(\.\d+)?,\d+(\.\d+)? \d+(\.\d+)?,\d+(\.\d+)? Z$/;
+  pathElements = pathElements
+    .filter((p) => p.hasAttribute("d"))
+    .filter((p) => polygonPathPattern.test(p.getAttribute("d")!));
+
+  const pointPathMapping = getPointPathMapping(pathElements);
+
+  const rectGlassPaths = Object.entries(pointPathMapping)
+    .map(([_, polygonPaths]) => polygonPaths)
+    .filter(isRectPathGlass);
+
+  rectGlassPaths.forEach((pathPolygons) =>
+    replacePathsWithRect(pathPolygons, groupSvg),
+  );
+};
+
+const replacePathsWithRect = (
+  polygons: SVGPathElement[],
+  groupSvg: SVGGElement,
+) => {
+  polygons.forEach((polygon) => {
+    groupSvg.removeChild(polygon);
+  });
+
+  const pathSvg = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path",
+  );
+
+  const getCoordinatesFromPathPolygon = (
+    svgPolygon: SVGPathElement,
+    index: number,
+  ) => {
+    const points = svgPolygon
+      .getAttribute("d")!
+      .split(" ")
+      [index]!.split(",")
+      .map((v) => v.replace("M", ""));
+    const pointX = parseFloat(points[0]);
+    const pointY = parseFloat(points[1]);
+    return { x: pointX, y: pointY };
+  };
+
+  const topLeftCoordinates = polygons.reduce(
+    (accumulator, svgPolygon) => {
+      let minCoords = accumulator;
+      const elementCoords0 = getCoordinatesFromPathPolygon(svgPolygon, 0);
+      if (elementCoords0.x <= minCoords.x && elementCoords0.y <= minCoords.y) {
+        minCoords = elementCoords0;
+      }
+
+      const elementCoords1 = getCoordinatesFromPathPolygon(svgPolygon, 1);
+      if (elementCoords1.x <= minCoords.x && elementCoords1.y <= minCoords.y) {
+        minCoords = elementCoords1;
+      }
+
+      const elementCoords2 = getCoordinatesFromPathPolygon(svgPolygon, 2);
+      if (elementCoords2.x <= minCoords.x && elementCoords2.y <= minCoords.y) {
+        minCoords = elementCoords2;
+      }
+
+      return minCoords;
+    },
+    getCoordinatesFromPathPolygon(polygons[0], 0),
+  );
+
+  const downRightCoordinates = polygons.reduce(
+    (accumulator, svgPolygon) => {
+      let minCoords = accumulator;
+      const elementCoords0 = getCoordinatesFromPathPolygon(svgPolygon, 0);
+      if (elementCoords0.x >= minCoords.x && elementCoords0.y >= minCoords.y) {
+        minCoords = elementCoords0;
+      }
+
+      const elementCoords1 = getCoordinatesFromPathPolygon(svgPolygon, 1);
+      if (elementCoords1.x >= minCoords.x && elementCoords1.y >= minCoords.y) {
+        minCoords = elementCoords1;
+      }
+
+      const elementCoords2 = getCoordinatesFromPathPolygon(svgPolygon, 2);
+      if (elementCoords2.x >= minCoords.x && elementCoords2.y >= minCoords.y) {
+        minCoords = elementCoords2;
+      }
+
+      return minCoords;
+    },
+    getCoordinatesFromPathPolygon(polygons[0], 0),
+  );
+  const x = topLeftCoordinates.x;
+  const y = topLeftCoordinates.y;
+  const width = downRightCoordinates.x - topLeftCoordinates.x;
+  const height = downRightCoordinates.y - topLeftCoordinates.y;
+  console.log("down: ", downRightCoordinates);
+  console.log("top: ", topLeftCoordinates);
+  pathSvg.setAttribute(
+    "d",
+    `M ${x} ${y} L ${x + width} ${y} L ${x + width} ${y + height} L ${x} ${
+      y + height
+    } Z`,
+  );
+
+  pathSvg.setAttribute("fill", polygons[0].getAttribute("fill")!);
+  pathSvg.setAttribute("stroke", polygons[0].getAttribute("stroke")!);
+  pathSvg.setAttribute(
+    "stroke-width",
+    polygons[0].getAttribute("stroke-width")!,
+  );
+
+  groupSvg.appendChild(pathSvg);
+};
+
+const isRectPathGlass = (pathPolygons: SVGPathElement[]) => {
+  if (pathPolygons.length !== 4) {
+    return false;
+  }
+
+  const innerPointPathMapping: { [key: string]: number } = {};
+  for (let i = 0; i < pathPolygons.length; i++) {
+    const polygonPoints = getPointsFromPathPolygon(pathPolygons[i]);
+    polygonPoints.forEach((p) => {
+      if (!(p in innerPointPathMapping)) {
+        innerPointPathMapping[p] = 0;
+      }
+      innerPointPathMapping[p] = innerPointPathMapping[p] + 1;
+    });
+  }
+
+  const areOddsPoints =
+    Object.entries(innerPointPathMapping).filter(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      ([_, count]) => count % 2 !== 0,
+    ).length > 0;
+  if (areOddsPoints) {
+    return false;
+  }
+  return true;
+};
+
+const getPointPathMapping = (pathPolygonElements: SVGPathElement[]) => {
+  const pointPathMapping: { [key: string]: SVGPathElement[] } = {};
+
+  for (let i = 0; i < pathPolygonElements.length; i++) {
+    const points = getPointsFromPathPolygon(pathPolygonElements[i]);
+    points.forEach((p) => {
+      if (!(p in pointPathMapping)) {
+        pointPathMapping[p] = [];
+      }
+      pointPathMapping[p].push(pathPolygonElements[i]);
+    });
+  }
+
+  return pointPathMapping;
+};
+
+const getPointsFromPathPolygon = (svgPolygon: SVGPathElement) => {
+  return svgPolygon
+    .getAttribute("d")!
+    .replace("Z", "")
+    .replace("M", "")
+    .split(" ");
 };
